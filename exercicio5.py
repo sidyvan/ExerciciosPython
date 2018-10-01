c55f1e09efdafb4cd7c2371da12d84b5cf40ac40
Sidyvan <sidvyan.dev@gmail.com>
2018-10-01 14:02:10 -0300

    Exercicio 5 e 6

---
+++ exercicio5.py
@@ -0,0 +1,3 @@
+#Faça um Programa que converta metros para centímetros. 
+metro = 2
+print(metro * 100)
---
+++ exercicio6.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
+#pi = 3.14159
+raio = int(input("infome o Raio"))
+print(3.14159 * (raio ** 2))
+
