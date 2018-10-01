bcbaf24e4d857373dc2b1cb64e0f61bb9a86f359
Sidyvan <sidvyan.dev@gmail.com>
2018-10-01 14:01:07 -0300

    Exercicio 4

---
+++ exercicio4.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+a = input("informe a primeira nota : ")
+b = input("informe a segunda nota")
+c = input("Informe a terçeira nota")
+d = input("Informe a quarta nota")
+print((int(a) + int(b) + int(c) + int(d)) / 4)
+
